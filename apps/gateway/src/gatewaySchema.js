2731d8487b9c99de9937dd86c002b08eac929316
Tomáš Horáček <tomas.horacek@gmail.com>
2022-10-10 18:29:10 +0200

    Add batchDelegateToSchema

--- apps/gateway/src/gatewaySchema.js
+++ apps/gateway/src/gatewaySchema.js
@@ -1,6 +1,9 @@
 import { delegateToSchema } from '@graphql-tools/delegate';
+import { batchDelegateToSchema } from '@graphql-tools/batch-delegate';
 import { stitchSchemas } from '@graphql-tools/stitch';
 import { getSubschemas } from './subschemas';
+
+const USE_BATCH_DELEGATE = true;
 
 export async function getGatewaySchema() {
   const { inventorySchema, marketingSchema } = await getSubschemas();
@@ -19,17 +22,32 @@ export async function getGatewaySchema() {
           resolve(parent, _args, context, info) {
             const bookId = parent.id;
 
-            return delegateToSchema({
-              schema: marketingSchema,
-              operation: 'query',
-              fieldName: 'reviews',
-              args: {
-                contentType: 'book',
-                objectId: bookId,
-              },
-              context,
-              info,
-            });
+            if (USE_BATCH_DELEGATE) {
+              return batchDelegateToSchema({
+                schema: marketingSchema,
+                operation: 'query',
+                fieldName: 'reviewsForObjectIds',
+                key: bookId,
+                argsFromKeys: (ids) => ({
+                  contentType: 'book',
+                  objectIds: ids,
+                }),
+                context,
+                info,
+              });
+            } else {
+              return delegateToSchema({
+                schema: marketingSchema,
+                operation: 'query',
+                fieldName: 'reviews',
+                args: {
+                  contentType: 'book',
+                  objectId: bookId,
+                },
+                context,
+                info,
+              });
+            }
           },
         },
       },
